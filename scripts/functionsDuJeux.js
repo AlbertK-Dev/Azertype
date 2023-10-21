ecbb90759883de2ed5d9601099821d8c352b49a0
AlbertK-Dev <147919300+AlbertK-Dev@users.noreply.github.com>
2023-10-21 17:46:02 +0100

    nettoyage de fichier de fonctions pour acceuillir les evènements

--- scripts/functionsDuJeux.js
+++ scripts/functionsDuJeux.js
@@ -2,7 +2,5 @@
  * la Fonction Main qui lance tous les autres
  */
 function lancerJeux() {
-  let score = 0;
-  let longueurDeLaListeChoisiParlUtilisateur = 0;
-  longueurDeLaListeChoisiParlUtilisateur = ListeDesMots.length;
+  return 0;
 }
