37446d1db07bb9a7b8dac3ca2b19abc066ea923d
AlbertK-Dev <147919300+AlbertK-Dev@users.noreply.github.com>
2023-10-25 12:38:47 +0100

    modification de la methode de récupération du score. Affichage dans la console. désormais, on récupère le mot proposer, la réponse de l'utilisateur,ainsi que qu'une valeur booleene indiquant si le mot est juste ou non.

--- scripts/fonctions.js
+++ scripts/fonctions.js
@@ -25,16 +25,32 @@ function lancerJeux() {
   let radioPhrases = document.querySelector("#Phrases");
   let nbreDeQuestionPoser = 0;
 
+  //déclarons un tableau qui contiendra le mot proposer
+  //ainsi que le mot saisi par l'utilisateur et idem pour les phrases
+  //j'ajoute aussi un booléen pour reussi et echoue afin de faciliter les opérations
+  //lors du traitement du tableau
+  let motsProposer = [];
+  for (let i = 0; i < motsProposer.length; i++) {
+    motsProposer[i] = [3];
+  }
+  let phrasesProposer = [];
+  for (let i = 0; i < phrasesProposer.length; i++) {
+    phrasesProposer[i] = [3];
+  }
+  let listeDeTextProposer;
+
   //ce qui à été sélectionné
 
   //valeur par defaut
   radioMots.checked = true;
   Tableau_A_Parcourir = [...ListeDesMots];
+  listeDeTextProposer = motsProposer;
   zoneScore.textContent = score + "/" + nbreDeQuestionPoser;
 
   radioMots.addEventListener("change", () => {
     if (radioMots.checked) {
       Tableau_A_Parcourir = [...ListeDesMots];
+      listeDeTextProposer = motsProposer;
       radioPhrases.checked = false;
       afficherProposition(Tableau_A_Parcourir[0]);
       //score = 0;
@@ -47,6 +63,7 @@ function lancerJeux() {
   radioPhrases.addEventListener("change", () => {
     if (radioPhrases.checked) {
       Tableau_A_Parcourir = [...ListeDesPhrases];
+      listeDeTextProposer = phrasesProposer;
       radioMots.checked = false;
       afficherProposition(Tableau_A_Parcourir[0]);
       // score = 0;
@@ -60,11 +77,18 @@ function lancerJeux() {
 
   btnValider.addEventListener("click", () => {
     nbreDeQuestionPoser++;
+    let textproposer = zoneMot.textContent;
+    let textsaisie = txtFrappeUser.value;
+    let trouver;
     if (txtFrappeUser.value === zoneMot.textContent) {
       score++;
+      trouver = true;
     } else {
       score = score;
+      trouver = false;
     }
+    let tabRecapitulatif = [textproposer, textsaisie, trouver];
+    listeDeTextProposer.push(tabRecapitulatif);
 
     txtFrappeUser.value = "";
     if (Tableau_A_Parcourir[indexDuMot + 1] === undefined) {
@@ -80,7 +104,26 @@ function lancerJeux() {
     }
     //mise à jour du score à chaque fois
     zoneScore.textContent = score + "/" + nbreDeQuestionPoser;
-
     indexDuMot++;
+
+    //statistiques
+    // let NombreDeQuestionPoser;
+    // let NombreDeMotPoser;
+    // let NombreDePhrasePoser;
+    // let NombreDeMotjuste;
+    // let NombreDePhraseJuste;
+    // let NombreDeMotFaux;
+    // let NombreDePhraseFaux;
+    // let listeDerreur;
+
+    //affichage des statistiques dans la console pour le moment
+    console.clear();
+    console.log("----> STATISTIQUES <----\n\n");
+    for (let i = 0; i < motsProposer.length; i++) {
+      console.log(motsProposer[i] + "\n");
+    }
+    for (let i = 0; i < phrasesProposer.length; i++) {
+      console.log(phrasesProposer[i] + "\n");
+    }
   });
 }
